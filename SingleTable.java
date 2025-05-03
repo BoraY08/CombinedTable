42706f06c865986ad4756a4c65dc7979ab14ceb2
BoraY08 <boray@nycstudents.net>
2025-05-03 21:23:41 +0000

    done

--- SingleTable.java
+++ SingleTable.java
@@ -1,25 +1,38 @@
 public class SingleTable {
+    private int seats;
+    private double viewQuality;
+    private int height;
+    public SingleTable(int s, double v, int h){
+        seats = s;
+        viewQuality = v;
+        height = h;
+    }
     /**
      * Returns the number of seats at this table. The value is always greater than
      * or equal to 4.
      */
     public int getNumSeats() {
-        /* implementation not shown */ }
+        return seats;
+    }
 
 
     /** Returns the height of this table in centimeters. */
     public int getHeight() {
-        /* implementation not shown */ }
+        return height;
+    }
 
 
     /** Returns the quality of the view from this table. */
     public double getViewQuality() {
-        /* implementation not shown */ }
+        return viewQuality;
+    }
+
 
 
     /** Sets the quality of the view from this table to value . */
     public void setViewQuality(double value) {
-        /* implementation not shown */ }
+        viewQuality = value; 
+    }
     // There may be instance variables, constructors, and methods that are not
     // shown.
 }
